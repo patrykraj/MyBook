bc2ae55fc1d4b8d3bd6da1af228546b374fbf9ea
Patryk Krajewski <patrykraj@interia.pl>
2020-08-14 16:46:28 +0200

    Implemented more tests for user reducer

--- src/store/reducers/user.test.js
+++ src/store/reducers/user.test.js
@@ -3,10 +3,10 @@ import * as actions from "../constants";
 
 describe("user reducer", () => {
   let initialState = {
-    token: null,
-    userId: null,
     error: null,
     loading: false,
+    token: null,
+    userId: null,
   };
 
   beforeEach(() => {
@@ -29,26 +29,48 @@ describe("user reducer", () => {
 
   it("should store the token upon login", () => {
     expect(
-      reducer(
-        {
-          token: null,
-          userId: null,
-          error: null,
-          loading: false,
+      reducer(undefined, {
+        type: actions.AUTH_SUCCESS,
+        payload: {
+          token: "token",
+          userId: "user-id",
         },
-        {
-          type: actions.AUTH_SUCCESS,
-          payload: {
-            token: "token",
-            userId: "user-id",
-          },
-        }
-      )
+      })
     ).toEqual({
+      ...initialState,
       token: "token",
       userId: "user-id",
-      error: null,
-      loading: false,
+    });
+  });
+
+  it("should remove token and userid on logout", () => {
+    expect(
+      reducer(undefined, {
+        type: actions.AUTH_LOGOUT,
+      })
+    ).toEqual(initialState);
+  });
+
+  it("should store error and stop loading on failure", () => {
+    expect(
+      reducer(undefined, {
+        type: actions.AUTH_FAILURE,
+        payload: "error",
+      })
+    ).toEqual({
+      ...initialState,
+      error: "error",
+    });
+  });
+
+  it("should start loading and reset error on start", () => {
+    expect(
+      reducer(undefined, {
+        type: actions.AUTH_START,
+      })
+    ).toEqual({
+      ...initialState,
+      loading: true,
     });
   });
 });
